9d8a13c4d881c300e3009d90814a0bd142dba51d
yashUcr773 <yagga004@ucr.edu>
2023-07-07 17:05:59 -0700

    projects section complete

--- src/app/v3/v3-hero/v3-hero.component.ts
+++ src/app/v3/v3-hero/v3-hero.component.ts
@@ -6,21 +6,22 @@ import Typed from 'typed.js';
   templateUrl: './v3-hero.component.html',
   styleUrls: ['./v3-hero.component.scss']
 })
-export class V3HeroComponent implements OnInit  {
+export class V3HeroComponent implements OnInit {
   ngOnInit(): void {
     var typed = new Typed('#typed', {
       stringsElement: '#typed-strings',
-      typeSpeed: 50,
       loop: true,
       smartBackspace: true,
-      backSpeed: 50,
+      typeSpeed: 30,
+      backSpeed: 30,
+      backDelay: 2000,
       shuffle: true,
       showCursor: false
     });
-    
+
   }
 
-  createRange(number:any){
+  createRange(number: any) {
     return new Array(number).fill(0)
       .map((n, index) => index + 1);
   }
--- src/app/v3/v3-home/v3-home.component.ts
+++ src/app/v3/v3-home/v3-home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from "aos";
 
 @Component({
   selector: 'app-v3-home',
   templateUrl: './v3-home.component.html',
   styleUrls: ['./v3-home.component.scss']
 })
-export class V3HomeComponent {
+export class V3HomeComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
 }
