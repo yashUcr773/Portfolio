2ab487d3c63e2a835de4dae44d5904ee128d2fb4
yashUcr773 <yagga004@ucr.edu>
2023-07-10 04:02:22 -0700

    added projects animations, footer and about

--- src/app/v3/v3-footer/v3-footer.component.ts
+++ src/app/v3/v3-footer/v3-footer.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-v3-footer',
   templateUrl: './v3-footer.component.html',
-  styleUrls: ['./v3-footer.component.scss']
+  styleUrls: ['./v3-footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class V3FooterComponent {
+export class V3FooterComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.setBubbles();
+  }
+
+  generateRandomNumbers(low: number, high: number, suffix: string) {
+    const randomNumber = (Math.random() * (high - low + 1)) + low;
+    return randomNumber + suffix
+  }
+
+  setBubbles() {
+
+    let bubbles: any = document.querySelector('.bubbles');
+    if (bubbles) {
+      for (let i = 0; i < 400; i += 1) {
+        let bubble: any = document.createElement('div');
+        let style = '--size: ' + this.generateRandomNumbers(2, 7, 'rem') + ';';
+        style += ' --distance: ' + this.generateRandomNumbers(5, 8, 'rem') + ';';
+        style += ' --position: ' + this.generateRandomNumbers(-10, 110, '%') + ';';
+        style += ' --time: ' + this.generateRandomNumbers(4, 6, 's') + ';';
+        style += ' --delay: ' + this.generateRandomNumbers(-1, 5, 's') + ';';
+        bubble.classList.add('bubble')
+        bubble.setAttribute('style', style)
+        bubbles.appendChild(bubble);
+      }
+    }
+
+  }
 
 }
--- src/app/v3/v3-projects/v3-projects.component.ts
+++ src/app/v3/v3-projects/v3-projects.component.ts
@@ -24,7 +24,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with",
         'github_link': 'https://github.com/yashUcr773/CS_224_Foundations_of_ML',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'ai'
+        'tags': 'ai',
+        'fadeStyle': fadeStyle['ai']
       },
       {
         'title': 'Project 2',
@@ -32,7 +33,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.",
         'github_link': 'https://github.com/yashUcr773/CS_171_Intro_to_ML',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'dsa'
+        'tags': 'dsa',
+        'fadeStyle': fadeStyle['dsa']
       },
       {
         'title': 'Project 3',
@@ -40,7 +42,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.",
         'github_link': 'https://github.com/yashUcr773/CS_228_Intro_to_DL',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'web'
+        'tags': 'web',
+        'fadeStyle': fadeStyle['web']
       },
       {
         'title': 'Project 4',
@@ -48,7 +51,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. ",
         'github_link': 'https://github.com/yashUcr773/CS_205_AI',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'web'
+        'tags': 'web',
+        'fadeStyle': fadeStyle['web']
       },
       {
         'title': 'Project 1',
@@ -56,7 +60,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with",
         'github_link': 'https://github.com/yashUcr773/CS_224_Foundations_of_ML',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'web'
+        'tags': 'web',
+        'fadeStyle': fadeStyle['web']
       },
       {
         'title': 'Project 2',
@@ -64,7 +69,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.",
         'github_link': 'https://github.com/yashUcr773/CS_171_Intro_to_ML',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'web'
+        'tags': 'web',
+        'fadeStyle': fadeStyle['web']
       },
       {
         'title': 'Project 3',
@@ -72,7 +78,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.",
         'github_link': 'https://github.com/yashUcr773/CS_228_Intro_to_DL',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'ai'
+        'tags': 'ai',
+        'fadeStyle': fadeStyle['ai']
       },
       {
         'title': 'Project 4',
@@ -80,7 +87,8 @@ export class V3ProjectsComponent implements OnInit {
         'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. ",
         'github_link': 'https://github.com/yashUcr773/CS_205_AI',
         'demo_link': 'https://www.cs.usfca.edu/~galles/visualization/RecQueens.html',
-        'tags': 'dsa'
+        'tags': 'dsa',
+        'fadeStyle': fadeStyle['dsa']
       }
     ]
   }
@@ -101,4 +109,11 @@ interface ProjectCards {
   github_link?: string;
   demo_link?: string;
   tags: 'all' | 'ai' | 'dsa' | 'web';
+  fadeStyle: fadeStyle;
+}
+
+enum fadeStyle {
+  ai = "fade-left",
+  web = "fade-right",
+  dsa = "fade-up"
 }
