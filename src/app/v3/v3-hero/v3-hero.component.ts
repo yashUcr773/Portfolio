b3811bf2db25dc349aa127ecc7baaf846a6ee494
yashUcr773 <yagga004@ucr.edu>
2023-07-12 16:29:26 -0700

    added test functionality to check routing to backend

--- src/app/v3/v3-hero/v3-hero.component.ts
+++ src/app/v3/v3-hero/v3-hero.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Typed from 'typed.js';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-v3-hero',
@@ -7,6 +8,9 @@ import Typed from 'typed.js';
   styleUrls: ['./v3-hero.component.scss']
 })
 export class V3HeroComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
   ngOnInit(): void {
     var typed = new Typed('#typed', {
       stringsElement: '#typed-strings',
@@ -24,6 +28,21 @@ export class V3HeroComponent implements OnInit {
   createRange(number: any) {
     return new Array(number).fill(0)
       .map((n, index) => index + 1);
+  }
+
+  makeCall() {
+    console.log('call made')
+    console.log('call made')
+    console.log('call made')
+    this.http.get('apigateway/data1').subscribe((response) => {
+      console.log(response);
+    });
+    this.http.get('apigateway/data2').subscribe((response) => {
+      console.log(response);
+    });
+    this.http.post('apigateway/data1', { 'body': 'text' }).subscribe((response) => {
+      console.log(response);
+    });
   }
 
 }
