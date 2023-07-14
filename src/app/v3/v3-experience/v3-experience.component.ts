10bf52daf425011bd3c832af847638ecd6ed267b
yashUcr773 <yagga004@ucr.edu>
2023-07-13 22:30:37 -0700

    added basis for about section. updated footer bubbles.

--- src/app/v3/v3-experience/v3-experience.component.ts
+++ src/app/v3/v3-experience/v3-experience.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import TagCloud from 'TagCloud';
 
 @Component({
   selector: 'app-v3-experience',
   templateUrl: './v3-experience.component.html',
   styleUrls: ['./v3-experience.component.scss']
 })
-export class V3ExperienceComponent {
+export class V3ExperienceComponent implements OnInit {
+  activeIndex = 0;
+  experiences: any = [];
+
+  ngOnInit(): void {
+    this.initializeWordClouds();
+    this.initExperiences();
+  }
+
+
+  initializeWordClouds() {
+    const myTags = [
+      'JavaScript', 'CSS', 'HTML',
+      'C', 'C++', 'React',
+      'Python', 'Java', 'git',
+      'django', 'Node.js', 'OpenCV',
+      'GCP', 'MySQL', 'jQuery',
+    ];
+
+    let element: any = document.querySelector('.word-cloud');
+    TagCloud(element, myTags, {
+      radius: 250,
+      maxSpeed: 'fast',
+      initSpeed: 'fast',
+      keep: true
+    });
+  }
+
+  updateActive(id: number) {
+
+    this.activeIndex = this.activeIndex == id ? 0 : id;
+
+  }
+
+  initExperiences() {
+    this.experiences = [
+      {
+        'Company': 'Compro Technologies',
+        'start': '1-July-2019',
+        'end': '25-July-2022',
+        'bulletsHeading': 'Roles and Responsibilities',
+        'bullets': [
+          'Increased growth by 200%',
+          'Increased Revenue by 300%',
+          'Reduced cost by 400%'
+        ],
+        'techHeading': 'Tools and Technologies',
+        'tech': ['AWS', 'Node', 'Angular', 'Express']
+      },
+      {
+        'Company': 'Compro Technologies',
+        'start': '1-July-2019',
+        'end': '25-July-2022',
+        'bulletsHeading': 'Roles and Responsibilities',
+        'bullets': [
+          'Increased growth by 200%',
+          'Increased Revenue by 300%',
+          'Reduced cost by 400%'
+        ],
+        'techHeading': 'Tools and Technologies',
+        'tech': ['AWS', 'Node', 'Angular', 'Express']
+      }
+    ]
+  }
 
 }
