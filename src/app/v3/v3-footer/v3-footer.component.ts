88f713e6274af4ea8c7ffe861a310513b9e6756c
yashUcr773 <yagga004@ucr.edu>
2023-07-19 21:02:27 -0700

    fix bubble flickering in mobile devices

--- src/app/v3/v3-footer/v3-footer.component.ts
+++ src/app/v3/v3-footer/v3-footer.component.ts
@@ -3,48 +3,49 @@ import { socials_config } from '../../../../config/socials';
 import { AppHelperService } from '../../services/app-helper.service';
 
 @Component({
-  selector: 'app-v3-footer',
-  templateUrl: './v3-footer.component.html',
-  styleUrls: ['./v3-footer.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-v3-footer',
+    templateUrl: './v3-footer.component.html',
+    styleUrls: ['./v3-footer.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class V3FooterComponent implements OnInit {
 
-  socials_config = socials_config
+    socials_config = socials_config
 
-  constructor(private appHelperService: AppHelperService) {
+    constructor(private appHelperService: AppHelperService) {
 
-  }
-  ngOnInit(): void {
-    this.setBubbles();
-  }
-
-  generateRandomNumbers(low: number, high: number, suffix: string) {
-    const randomNumber = this.appHelperService.generateRandomNumbers(low, high);
-    return randomNumber + suffix;
-  }
-
-  setBubbles() {
-
-    let bubbles: any = document.querySelector('.bubbles');
-    if (bubbles) {
-      for (let i = 0; i < 500; i += 1) {
-        let bubble: any = document.createElement('div');
-        let style = '--size: ' + this.generateRandomNumbers(1, 10, 'rem') + ';';
-        style += ' --distance: ' + this.generateRandomNumbers(4, 12, 'rem') + ';';
-        style += ' --position: ' + this.generateRandomNumbers(-10, 110, '%') + ';';
-        style += ' --time: ' + this.generateRandomNumbers(2, 8, 's') + ';';
-        style += ' --delay: ' + this.generateRandomNumbers(-1, 8, 's') + ';';
-        bubble.classList.add('bubble');
-        bubble.setAttribute('style', style);
-        bubbles.appendChild(bubble);
-      }
+    }
+    ngOnInit(): void {
+        this.setBubbles();
     }
 
-  }
+    generateRandomNumbers(low: number, high: number, suffix: string) {
+        const randomNumber = this.appHelperService.generateRandomNumbers(low, high);
+        return randomNumber + suffix;
+    }
 
-  openLink(url: string) {
-    this.appHelperService.launchLink(url);
-  }
+    setBubbles() {
+
+        let totalBubbles = Math.min(window.innerWidth / 3, 100);
+        let bubbles: any = document.querySelector('.bubbles');
+        if (bubbles) {
+            for (let i = 0; i < totalBubbles; i += 1) {
+                let bubble: any = document.createElement('div');
+                let style = '--size: ' + this.generateRandomNumbers(1, 10, 'rem') + ';';
+                style += ' --distance: ' + this.generateRandomNumbers(4, 12, 'rem') + ';';
+                style += ' --position: ' + this.generateRandomNumbers(-10, 110, '%') + ';';
+                style += ' --time: ' + this.generateRandomNumbers(2, 8, 's') + ';';
+                style += ' --delay: ' + this.generateRandomNumbers(-1, 8, 's') + ';';
+                bubble.classList.add('bubble');
+                bubble.setAttribute('style', style);
+                bubbles.appendChild(bubble);
+            }
+        }
+
+    }
+
+    openLink(url: string) {
+        this.appHelperService.launchLink(url);
+    }
 
 }
