dab5da5e540ca1096a919cfd66d56e0c9993ab6f
yashUcr773 <yagga004@ucr.edu>
2023-07-23 12:53:50 -0700

    header refactor

--- src/app/v3/v3-header/v3-header.component.ts
+++ src/app/v3/v3-header/v3-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AppHelperService } from '../../services/app-helper.service';
 import { socials_config } from 'config/socials';
 
@@ -11,6 +11,7 @@ import { socials_config } from 'config/socials';
 export class V3HeaderComponent implements OnInit {
 
     socials_config = socials_config;
+    hamburgerActive = false;
     constructor(private appHelperService: AppHelperService) {
 
     }
@@ -19,16 +20,38 @@ export class V3HeaderComponent implements OnInit {
 
     }
 
+    @HostListener('window:resize', ['$event'])
+    resizeHandler() {
+        if (window.innerWidth > 600) {
+            this.toggleHamburger(false);
+        }
+    }
+
     goto(section: string) {
+        this.toggleHamburger(false);
         let element = document.querySelector('#' + section + "-section");
         element?.scrollIntoView({ behavior: 'smooth' });
-
-        let hamburger: any = document.querySelector('#nav-check');
-        hamburger.checked = false;
     }
 
     launchLink(url: string) {
+        this.toggleHamburger(false);
         this.appHelperService.launchLink(url);
+    }
+
+    toggleHamburger(toggleValue?: boolean) {
+
+        if (toggleValue != undefined) {
+            this.hamburgerActive = toggleValue;
+        } else {
+            this.hamburgerActive = !this.hamburgerActive;
+        }
+
+        if (this.hamburgerActive == false) {
+            document.querySelector('body')?.classList.remove('sidebarActive')
+        } else {
+            document.querySelector('body')?.classList.add('sidebarActive')
+        }
+
     }
 
 }
