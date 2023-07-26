351e0754fb4c8586bb114e93581146bb1abf6cd0
yashUcr773 <yagga004@ucr.edu>
2023-07-25 19:09:23 -0700

    moved external link to body added option to not open in new tab for external link added projects page added n-puzzle solver page moved aos init to main app

---
+++ src/app/Projects/projects-home/projects-home.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { AppHelperService } from 'src/app/services/app-helper.service';
+
+@Component({
+    selector: 'app-projects-home',
+    templateUrl: './projects-home.component.html',
+    styleUrls: ['./projects-home.component.scss']
+})
+export class ProjectsHomeComponent {
+
+    project_cards: ProjectCards[] = [];
+    all_projects: ProjectCards[] = [];
+    display_cards = 'all';
+
+    ngOnInit(): void {
+        this.initializeCards();
+    }
+
+    constructor(private appHelperService: AppHelperService) {
+
+    }
+
+    initializeCards() {
+
+        this.all_projects = [
+            {
+                'title': 'Portfolio Website',
+                'image': '../../../assets/Project_images/portfolio_screenshot.jpg',
+                'description': "Website telling about me and showcasing various projects I have been a part of.",
+                'github_link': 'https://github.com/yashUcr773/Portfolio',
+                'demo_link': 'https://yashaggarwal.com/',
+                'tags': ['ai', 'web'],
+                'fadeStyle': fadeStyle['web']
+            }
+        ]
+
+        this.project_cards = this.all_projects;
+    }
+
+    filterCards(type: AllowedTags) {
+        this.display_cards = type;
+
+        this.project_cards = this.all_projects.filter(elem => {
+            if (type == 'all') return true
+            return elem.tags.indexOf(type) > -1
+        })
+    }
+
+    launchLink(url: string) {
+        this.appHelperService.launchLink(url, false);
+    }
+
+}
+
+interface ProjectCards {
+    title: string;
+    image: string;
+    description: string;
+    github_link?: string;
+    demo_link?: string;
+    tags: AllowedTags[];
+    fadeStyle: fadeStyle;
+}
+
+type AllowedTags = 'all' | 'ai' | 'dsa' | 'web';
+
+enum fadeStyle {
+    ai = "fade-left",
+    web = "fade-right",
+    dsa = "fade-up"
+}
---
+++ src/app/Projects/projects-list/n-puzzle-solver/n-puzzle-solver.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-n-puzzle-solver',
+  templateUrl: './n-puzzle-solver.component.html',
+  styleUrls: ['./n-puzzle-solver.component.scss']
+})
+export class NPuzzleSolverComponent {
+
+}
