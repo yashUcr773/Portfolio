647a1e7f1a6a08321378f9bde034b08ea6445e2e
yashUcr773 <yagga004@ucr.edu>
2024-04-06 17:37:57 -0700

    add resume component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { V3BaseComponent } from './v3/v3-base/v3-base.component';
 import { ProjectsHomeComponent } from './Projects/projects-home/projects-home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 const routes: Routes = [
     { path: '', component: V3BaseComponent },
     { path: 'projects', component: ProjectsHomeComponent },
+    { path: 'resume', component: ResumeComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
