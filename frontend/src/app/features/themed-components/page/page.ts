a5d20708899d9668b1d7954e52c71e6fe0d06705
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2026-01-01 16:00:41 +0530

    next page component

--- frontend/src/app/features/themed-components/page/page.ts
+++ frontend/src/app/features/themed-components/page/page.ts
@@ -3,9 +3,10 @@ import { Socials } from '../../../shared/ui/socials/socials';
 import { SocialPicker } from '../../../shared/ui/social-picker/social-picker';
 import { Logos } from '../../../shared/ui/logos/logos';
 import { SectionHeader } from '../../../shared/ui/section-header/section-header';
+import { NextComponentLoader } from '../../../shared/ui/next-component-loader/next-component-loader';
 @Component({
   selector: 'app-themed-components-page',
-  imports: [Socials, SocialPicker, Logos, SectionHeader],
+  imports: [Socials, SocialPicker, Logos, SectionHeader, NextComponentLoader],
   standalone: true,
   templateUrl: './page.html',
   styleUrl: './page.scss',
---
+++ frontend/src/app/shared/ui/next-component-loader/next-component-loader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-component-loader',
+  imports: [],
+  templateUrl: './next-component-loader.html',
+  styleUrl: './next-component-loader.scss',
+})
+export class NextComponentLoader {
+
+}
