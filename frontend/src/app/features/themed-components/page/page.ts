c072c9cc71b3eed3c020390c11f25fcbbf17b627
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2026-01-01 15:54:50 +0530

    section header

--- frontend/src/app/features/themed-components/page/page.ts
+++ frontend/src/app/features/themed-components/page/page.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Socials } from '../../../shared/ui/socials/socials';
 import { SocialPicker } from '../../../shared/ui/social-picker/social-picker';
 import { Logos } from '../../../shared/ui/logos/logos';
+import { SectionHeader } from '../../../shared/ui/section-header/section-header';
 @Component({
   selector: 'app-themed-components-page',
-  imports: [Socials, SocialPicker, Logos],
+  imports: [Socials, SocialPicker, Logos, SectionHeader],
   standalone: true,
   templateUrl: './page.html',
   styleUrl: './page.scss',
---
+++ frontend/src/app/shared/ui/section-header/section-header.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  imports: [],
+  templateUrl: './section-header.html',
+  styleUrl: './section-header.scss',
+})
+export class SectionHeader {
+  @Input({ required: true })
+  heading: string = ""
+}
